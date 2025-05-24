b953b42a7ea06c8fa37141dd491fa78cc7cd10a3
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-05-24 21:15:11 +0530

    REPL Read-Eval-Print Loop

--- app/main.go
+++ app/main.go
@@ -8,12 +8,13 @@ import (
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
-	// Wait for user input
 }
