b0ef1e4c509622911649440d04decbe64bdc8097
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-05-24 21:12:48 +0530

    Invalid Command

--- app/main.go
+++ app/main.go
@@ -9,7 +9,11 @@ import (
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
-
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
 }
