d020aaced3fe4de51b890f5fe086c663d098a584
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-05-25 07:20:27 +0530

    🏠

--- builtin/cd.go
+++ builtin/cd.go
@@ -13,6 +13,10 @@ type Cd struct {
 func (e *Cd) Handle(args []string) {
 	dir := args[0]
 
+	if dir == "~" {
+		dir = os.Getenv("HOME")
+	}
+
 	if !utils.IsValidPath(dir) {
 		fmt.Printf("cd: %s: No such file or directory\n", args[0])
 	}
