ea58a0580b259cb1d02ebfaf332ded346116ce10
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-05-25 07:04:55 +0530

    fix: CD

--- builtin/cd.go
+++ builtin/cd.go
@@ -1,6 +1,7 @@
 package builtin
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -8,7 +9,23 @@ type Cd struct {
 }
 
 func (e *Cd) Handle(args []string) {
-	os.Chdir(args[0])
+	dir := args[0]
+
+	entries, err := os.ReadDir("./")
+
+	if err != nil {
+		fmt.Println("Cannot access directories")
+		return
+	}
+
+	for _, e := range entries {
+		if dir == e.Name() {
+			os.Chdir(args[0])
+			return
+		}
+	}
+
+	fmt.Printf("cd: %s: No such file or directory\n", args[0])
 }
 
 func (e *Cd) GetCmdName() string {
